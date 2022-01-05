9dd042d2855b65d1a4223773319a31d9c2b9b52a
tiana <tianasjohnson98@gmail.com>
2022-01-05 15:49:31 -0500

    routes

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const path = require("path");
 const express = require("express");
 
+const exphbs = require("express-handlebars");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Express middleware
+const hbs = exphbs.create({});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+
+const routes = require("./controllers/");
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
