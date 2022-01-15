f9f61037fcf37808185c2b906b7ffc6e7070968e
Edelgard <jtboyman@gmail.com>
2022-01-15 10:44:35 -0500

    homepage base

---
+++ controllers/home-routes.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const {Team, Admin, Task, User} = require('../models')
+
+//GET all teams for the homepage
+router.get('/', (req, res) => {
+    Team.findAll({
+        attributes: ['id', 'team_name', 'team_description', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM point WHERE team.id = point.team_id)'), 'point_count']],
+        include: [
+            {
+                model: Task,
+                attributes: ['id', 'task_text', 'admin_id', 'team_id', 'created_at'],
+                include: {
+                    model: Admin,
+                    attributes: ['admin_name']
+                }
+            },
+            {
+                model: Admin,
+                attributes: ['admin_name']
+            }
+        ]
+    })
+    .then(dbTeamData => {
+        const teams = dbTeamData.map(team => team.get({plain: true}));
+
+        res.render('homepage', {
+            teams
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
