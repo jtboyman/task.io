2e6dfcfa1b5e7747ad5e980b370c2a4a792711f4
tiana <tianasjohnson98@gmail.com>
2022-01-15 01:14:15 -0500

    task model

--- models/Task.js
+++ models/Task.js
@@ -2,3 +2,50 @@ const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
 class Task extends Model {}
+
+Task.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    task_title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+
+    task_description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+    admin_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "admin",
+        key: "id",
+      },
+    },
+    team_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "team",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+  }
+);
+
+module.exports = Task;
