b45eae3d99ddeaac348b85eaf6277cc00a7246ee
Edelgard <jtboyman@gmail.com>
2022-01-16 13:44:50 -0500

    cool partials and formatting

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const exphbs = require('express-handlebars');
 const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -23,7 +24,7 @@ app.use(session(sess));
 
 
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({helpers});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
